4a4606f7d7c8916ddb50a7d2ede93f632af6b27f
Bryan <160513922+bryanerjunyet@users.noreply.github.com>
2025-10-10 20:22:48 +0800

    Completed visualisation

---
+++ multichart/js/vega_lite_vis.js
@@ -0,0 +1,11 @@
+(function () {
+  const embedOptions = { actions: false };
+
+  vegaEmbed('#choropleth_map', 'js/malaysia_crime_map.vg.json', embedOptions)
+    .then(() => {})
+    .catch(console.error);
+
+  vegaEmbed('#crime_stack', 'js/malaysia_crime_stacked.vg.json', embedOptions)
+    .then(() => {})
+    .catch(console.error);
+})();
